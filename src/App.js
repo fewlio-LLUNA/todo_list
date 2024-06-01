a96c4a09a66a38c070e47567545b59dc852f52fe
fewlio-LLUNA <seraja0824ryoiki@gmail.com>
2024-06-01 21:20:31 +0900

    2割ぐらいまだ理解できていないけれどTodoリストを作ってみた

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
+import { useState } from 'react'; //useStateを扱うためにimportする
 import './App.css';
 
+
 function App() {
-  return (
+
+  /*todos（Todoリスト）とsetTodos（todosを更新する関数）を定義
+    input（入力フィールドの値）とsetInput（inputを更新する関数）を定義 */
+  const [todos, setTodos] = useState([]) //[]ってなに???????????????????????
+  const [input, setInput] = useState('') //文字列がない状態をデフォルトとする
+
+  /*todoを追加する関数*/
+  const addTodo = (e) => { //(e)を指定することでイベントオブジェクトの指定ができる？
+    e.preventDefault()
+    if (input.trim()) { // 入力が空でないか確認
+    setTodos([...todos, input]); // 新しいTodoを追加
+    setInput(''); // 入力フィールドをクリア
+    }
+  }
+  
+
+/*追加されたTodoを表示する関数*/
+function DisplayTodo(){ 
+  return(
+    <li>{todos}</li>
+  )
+}
+
+
+  return (//return:画面に返す要素
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Todo-List With React</h1><br></br>
+
+      <form className='add' onSubmit={addTodo}>
+      <label>タスクを追加</label>
+      <input 
+        type='text' 
+        name='add' 
+        placeholder='やるべきことは何？'
+        /*以下2行がよくわからない */
+        value={input}
+        onChange={(e) => setInput(e.target.value)} 
+        />
+      </form>
+      
+      <button onClick={addTodo}>追加</button>
+
+      <ul>
+        {todos.map(function(todo, index){
+          return<DisplayTodo key={index} todo={todos}/>
+        })}
+      </ul>
+      
+
     </div>
   );
 }
