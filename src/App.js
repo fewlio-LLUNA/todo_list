23aa5fa9581ff040b21bf165239cbd736ff1480d
fewlio-LLUNA <seraja0824ryoiki@gmail.com>
2024-06-13 17:03:38 +0900

    localStrageでデータを保存可能にした

--- src/App.js
+++ src/App.js
@@ -4,8 +4,16 @@ import "./App.css";
 function App() {
   /*todos（Todoリスト）とsetTodos（todosを更新する関数）を定義
     input（入力フィールドの値）とsetInput（inputを更新する関数）を定義 */
-  const [todos, setTodos] = useState([]); //todos = []
-  const [input, setInput] = useState(""); //input - ""
+    const [todos, setTodos] = useState(() => {
+      const savedTodos = localStorage.getItem("todos");// LocalStorageから保存されているtodosを取得      
+      if (savedTodos) {                                // 保存されているデータがあるかどうかを確認
+        return JSON.parse(savedTodos);                 // データがある場合はJSON配列→JSの配列に変換
+      } else {
+        return [];
+      }
+    });
+    
+  const [input, setInput] = useState(""); //input ＝ ""
 
   /*todoを追加する関数*/
   const addTodo = (e) => {
@@ -23,16 +31,17 @@ function App() {
     setTodos(todos.filter((_, i) => i !== index));
   };
 
+  /* 依存配列[todos]が変更されるたびに発火する */
   useEffect(() => {
-    console.log(todos);
+    localStorage.setItem("todos", JSON.stringify(todos));// データがある場合はJSの配列→JSON配列に変換
   }, [todos]);
-
+  
   /*追加されたTodoを表示する関数*/
   function DisplayTodo({ todo, index, removeTodo }) {
     return (
       <div className="DisplayTodo">
       <li>
-        <p>タスク名：{todo}</p>
+        <p>・{todo}</p>
         <button onClick={() => removeTodo(index)}>完了</button>
       </li>
       </div>
